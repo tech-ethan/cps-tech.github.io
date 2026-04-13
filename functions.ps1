9be6be6ab0c97052ac8c12527cb4dda5caabc1c4
tech-ethan <ethan.austin@cps.k12.ar.us>
2026-04-13 11:42:22 -0500

    Update functions.ps1

--- functions.ps1
+++ functions.ps1
@@ -15,16 +15,20 @@ function Assert-Administrator {
 }
 
 function Is-Installed {
-    param (
-        [Parameter(Mandatory)]
-        [string]$DisplayName
-    )
+    param ([string]$DisplayName)
 
-    Get-ItemProperty `
+    foreach ($key in Get-ItemProperty `
         HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, `
         HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* `
-        -ErrorAction SilentlyContinue |
-        Where-Object { $_.DisplayName -like "*$DisplayName*" }
+        -ErrorAction SilentlyContinue) {
+
+        if ($key.PSObject.Properties['DisplayName'] -and
+            $key.DisplayName -like "*$DisplayName*") {
+            return $true
+        }
+    }
+
+    return $false
 }
 
 function Install-Exe {
