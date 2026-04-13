529da0a5005fa2a64e7a37f30d99ce3e25992f39
tech-ethan <ethan.austin@cps.k12.ar.us>
2026-04-13 11:50:24 -0500

    Update functions.ps1

--- functions.ps1
+++ functions.ps1
@@ -60,21 +60,6 @@ function Install-Exe {
     Start-Process -FilePath $file -ArgumentList $Args -Wait -NoNewWindow
 }
 
-
-    if (Is-Installed $Name) {
-    Write-Host "$Name is already installed — skipping." -ForegroundColor Yellow
-    return
-}
-
-    $file = Join-Path $env:TEMP ($Name -replace '\s','_') + ".exe"
-
-    Write-Host "Downloading $Name..." -ForegroundColor Cyan
-    Invoke-WebRequest -Uri $Url -OutFile $file -UseBasicParsing
-
-    Write-Host "Installing $Name..." -ForegroundColor Green
-    Start-Process -FilePath $file -ArgumentList $Args -Wait -NoNewWindow
-}
-
 function Install-Winget {
     param (
         [Parameter(Mandatory)]
